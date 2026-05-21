37616c41ab15c0027991bc5f0b88476101e78b79
wipip <166628878+wipip@users.noreply.github.com>
2026-05-21 06:12:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ st.set_page_config(
 # ==========================================
 BROKER = "broker.mqttdashboard.com"
 PORT = 1883
-TOPIC_RECEIVE = "Sensor/THP2"
+TOPIC_RECEIVE = "Sensor/THP3"
 TOPIC_SEND = "cmqtt_s"
 
 if 'sensor_data' not in st.session_state:
